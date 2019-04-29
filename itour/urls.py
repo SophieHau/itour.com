cce03df1ba419799fa93253c5e812de6b2851a5c
Gary Waller <garywaller246@gmail.com>
2019-04-29 16:34:14 +0300

    end of monday

--- itour/urls.py
+++ itour/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-	path ('booking/', include('booking.urls')),
+	path('booking/', include('booking.urls')),
     path('admin/', admin.site.urls),
     path('', include('tour.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
