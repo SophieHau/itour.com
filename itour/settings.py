c1939086f20314e4dd26d65dc6162aebf03793e2
Selmo <45966506+selmoka@users.noreply.github.com>
2019-04-30 09:59:47 +0300

    Changed urls

--- itour/settings.py
+++ itour/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'profileapp.apps.ProfileappConfig',
+    'tour',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static_files")
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
