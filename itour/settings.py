f91fa6c2c4bc855a1ed582207b180dfb6e30197d
Selmo <45966506+selmoka@users.noreply.github.com>
2019-04-30 10:11:37 +0300

    Gitignore and settings changed

--- itour/settings.py
+++ itour/settings.py
@@ -133,10 +133,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-<<<<<<< HEAD
-=======
 
->>>>>>> profile
 STATIC_ROOT = os.path.join(BASE_DIR, "static_files")
 
 STATICFILES_DIRS = (
