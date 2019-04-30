c7c8675671efc565345da439a9dd818f4fe28f7b
Selmo <45966506+selmoka@users.noreply.github.com>
2019-04-30 11:41:29 +0300

    Password reset email to console

--- itour/settings.py
+++ itour/settings.py
@@ -27,15 +27,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # During development only
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  
+# During development only
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'selmok'
-EMAIL_HOST_PASSWORD = 'Sendg9411'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'selmok@hotmail.com'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.sendgrid.net'
+# EMAIL_HOST_USER = 'selmok'
+# EMAIL_HOST_PASSWORD = 'Mypassword!'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# DEFAULT_FROM_EMAIL = 'selmok@hotmail.com'
 
 # LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = 'signin'
