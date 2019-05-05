12735ad5f9a5a04233082f8ffd7694550c228bd4
Selmo <45966506+selmoka@users.noreply.github.com>
2019-05-05 12:33:14 +0300

    Fixed review templates

--- review/models.py
+++ review/models.py
@@ -14,9 +14,9 @@ class Review(models.Model):
 )
 
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	booking = models.ForeignKey(Booking, on_delete=models.CASCADE, default=1)
+	booking = models.ForeignKey(Booking, on_delete=models.CASCADE, default=1, related_name='review_booking')
 	text = models.CharField(default='', blank=True, null=True, max_length = 100)
 	rating = models.CharField(choices=RATING_CHOICES, default="good", max_length=9)
 	date = models.DateTimeField(auto_now_add=True)	
 	def __str__(self):
-		return self.user.username + ' - ' + self.tour.name
+		return self.user.username + ' - ' + str(self.date)
