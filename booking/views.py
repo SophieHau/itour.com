b47b29776034e740c3a63351cb1d80e2b3d3588f
Selmo <45966506+selmoka@users.noreply.github.com>
2019-05-06 13:52:34 +0300

    Show reviews on load page

--- booking/views.py
+++ booking/views.py
@@ -15,7 +15,7 @@ def create(request, tour_id=None):
     else:
         tour = Tour.objects.first()
     form = BookingForm(initial={
-         
+            'tour': tour,
             'price': tour.price, 
             'number_of_people': 1
         })
